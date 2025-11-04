ae8ed2f6565f772520b94834b86a605494a38aa3
Ankita Dhar <ankita.dhar@jora.com>
2025-11-05 09:49:54 +1100

    added appropriate progress report prints

--- main.py
+++ main.py
@@ -17,7 +17,11 @@ def main():
     else: 
         file_path = args.inputfile
 
+    print("Analyzing traffic data...")
+
     traffic_analyzer = TrafficAnalyzer(file_path)
+
+    print("Generating traffic analysis report...")
 
     traffic_analysis_result = TrafficAnalysisResult(
         total_traffic = traffic_analyzer.calculate_traffic(),
